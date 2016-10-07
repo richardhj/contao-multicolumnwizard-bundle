2fbd989d71ec06a7b40a7f9fa3f6fd3613172268
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2016-10-07 21:27:57 +0200

    Language

---
+++ module/languages/de/tl_nc_notification.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['ferienpass'] = 'Ferienpass';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['offer_al_status_change'][0] = 'Statusänderung Teilnehmerliste';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['offer_al_status_change'][1] = 'Dieser Benachrichtigungstyp wird nach einer Änderung bei der Teilnehmerliste versendet.';
