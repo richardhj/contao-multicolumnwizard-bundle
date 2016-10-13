fcd633cdd2d082a87ce16cd554fec183419e2720
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2016-10-14 00:25:59 +0200

    Init pimple

---
+++ module/config/services.php
@@ -0,0 +1,15 @@
+<?php
+
+$container['ferienpass.applicationsystem.firstcome'] = function ($container) {
+    return new Ferienpass\ApplicationSystem\FirstCome();
+};
+
+$container['ferienpass.applicationsystem.lot'] = function ($container) {
+    return new Ferienpass\ApplicationSystem\Lot();
+};
+
+$container['ferienpass.applicationsystem'] = $container->share(
+    function ($container) {
+        return $container['ferienpass.applicationsystem.lot'];
+    }
+);
