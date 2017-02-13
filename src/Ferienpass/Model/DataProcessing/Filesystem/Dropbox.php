7f54377e7dbd284ad42fb5d530d1cedb34cd57a0
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-02-13 07:58:18 +0100

    File structure DataProcessing

---
+++ src/Ferienpass/Model/DataProcessing/Filesystem/Dropbox.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * FERIENPASS extension for Contao Open Source CMS built on the MetaModels extension
+ *
+ * Copyright (c) 2015-2017 Richard Henkenjohann
+ *
+ * @package Ferienpass
+ * @author  Richard Henkenjohann <richard@ferienpass.online>
+ */
+
+
+namespace Ferienpass\Model\DataProcessing\Filesystem;
+
+
+use Ferienpass\Model\DataProcessing\FilesystemInterface;
+
+class Dropbox implements FilesystemInterface
+{
+
+}
---
+++ src/Ferienpass/Model/DataProcessing/Format/Xml.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * FERIENPASS extension for Contao Open Source CMS built on the MetaModels extension
+ *
+ * Copyright (c) 2015-2017 Richard Henkenjohann
+ *
+ * @package Ferienpass
+ * @author  Richard Henkenjohann <richard@ferienpass.online>
+ */
+
+
+namespace Ferienpass\Model\DataProcessing\Format;
+
+
+use Ferienpass\Model\DataProcessing\FormatInterface;
+
+class Xml implements FormatInterface
+{
+
+}
