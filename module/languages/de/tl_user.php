af45ea74c7d3241da5533c3367285d557d2e8c57
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2017-04-09 18:34:29 +0200

    Add toggle checkbox to tl_user

---
+++ module/languages/de/tl_user.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * FERIENPASS extension for Contao Open Source CMS built on the MetaModels extension
+ *
+ * Copyright (c) 2015-2017 Richard Henkenjohann
+ *
+ * @package Ferienpass
+ * @author  Richard Henkenjohann <richard@ferienpass.online>
+ */
+
+
+/** @noinspection PhpUndefinedMethodInspection */
+$table = UserModel::getTable();
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG'][$table]['offer_date_picker'][0] = 'Date-Picker für Angebote';
+$GLOBALS['TL_LANG'][$table]['offer_date_picker'][1] = 'Das Date-Period-Picker-Widget für die Ferienpass-Angebote verwenden.';
