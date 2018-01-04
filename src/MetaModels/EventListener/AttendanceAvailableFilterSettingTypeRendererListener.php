699e6315555fc84c6bc636f771396d9ae243dc2a
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-01-04 14:04:59 +0100

    Add model-to-label listeners

---
+++ src/MetaModels/EventListener/AttendanceAvailableFilterSettingTypeRendererListener.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of richardhj/contao-ferienpass.
+ *
+ * Copyright (c) 2015-2018 Richard Henkenjohann
+ *
+ * @package   richardhj/richardhj/contao-ferienpass
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2015-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/richardhj/contao-ferienpass/blob/master/LICENSE
+ */
+
+namespace Richardhj\ContaoFerienpassBundle\MetaModels\EventListener;
+
+use ContaoCommunityAlliance\DcGeneral\Data\ModelInterface;
+use ContaoCommunityAlliance\DcGeneral\EnvironmentInterface;
+use MetaModels\CoreBundle\EventListener\DcGeneral\Table\FilterSetting\AbstractFilterSettingTypeRenderer;
+
+/**
+ * Handles rendering of model from tl_metamodel_filtersetting.
+ */
+class AttendanceAvailableFilterSettingTypeRendererListener extends AbstractFilterSettingTypeRenderer
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTypes()
+    {
+        return ['attendance_available'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getLabelParameters(EnvironmentInterface $environment, ModelInterface $model)
+    {
+        return $this->getLabelParametersWithAttributeAndUrlParam($environment, $model);
+    }
+}
