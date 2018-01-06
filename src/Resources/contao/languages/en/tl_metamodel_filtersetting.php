31685b9955ccfe92f82e68153682fd6e4c864f31
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-01-07 00:17:18 +0100

    Translate en

---
+++ src/Resources/contao/languages/en/tl_metamodel_filtersetting.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of richardhj/contao-ferienpass.
+ *
+ * Copyright (c) 2015-2018 Richard Henkenjohann
+ *
+ * @package   richardhj/contao-ferienpass
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2015-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/contao-ferienpass/blob/master/LICENSE
+ */
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['age']                  = 'Age';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['attendance_available'] = 'Attendance available';
