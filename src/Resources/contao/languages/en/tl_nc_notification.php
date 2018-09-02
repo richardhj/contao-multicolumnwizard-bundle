14fc895b403df3dfb9184c87468d5f75490d3795
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-09-02 18:40:51 +0200

    Translations.

---
+++ src/Resources/contao/languages/en/tl_nc_notification.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of richardhj/contao-ferienpass.
+ *
+ * Copyright (c) 2015-2018 Richard Henkenjohann
+ *
+ * @package   richardhj/contao-ferienpass
+ * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
+ * @copyright 2015-2018 Richard Henkenjohann
+ * @license   https://github.com/richardhj/contao-ferienpass/blob/master/LICENSE
+ */
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['ferienpass'] = 'Ferienpass';
+
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['application_list_status_change'][0] = 'Status change application list';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['application_list_status_change'][1] = 'This notification type is sent after change on application list.';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['applications_member_overview'][0]   = 'Applications overview';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['applications_member_overview'][1]   = 'This notification contains an overview over all applications of one members participants.';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['application_list_reminder'][0]      = 'Application reminder';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['application_list_reminder'][1]      = 'This application reminds one participant prior the event.';
