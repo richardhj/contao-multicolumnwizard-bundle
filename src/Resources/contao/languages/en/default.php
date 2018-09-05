ce0efb31b7b2a472db667f856a0ae2fb4463f521
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2018-09-05 12:33:41 +0200

    Translations + CS.

--- src/Resources/contao/languages/en/default.php
+++ src/Resources/contao/languages/en/default.php
@@ -19,4 +19,10 @@ $GLOBALS['TL_LANG']['MSC']['attendance_status']['error']      = 'Not attending';
 $GLOBALS['TL_LANG']['MSC']['application_system']['firstcome'] = 'First Come';
 $GLOBALS['TL_LANG']['MSC']['application_system']['lot']       = 'Lot';
 
-$GLOBALS['TL_LANG']['MSC']['ferienpass_erase_personal_data']['main_headline'] = 'Dieses Tool steht Ihnen zur Verfügung, um alle personenbezogenen Daten der registrierten Eltern zu löschen.';
+$GLOBALS['TL_LANG']['MSC']['welcome_gantt']['task_description']['application_system']['lot']       = 'The Lot application system is present. The parents do not get any application confirmation, rather you have to assign all applications. Without any appliation system being present, neither applications nor cancellations are possible!';
+$GLOBALS['TL_LANG']['MSC']['welcome_gantt']['task_description']['application_system']['firstcome'] = 'The First Come application system is present. The parents become a application confirmation immediately and the waiting list is handled on the fly. You do not have to intervene in this procedure. Without any appliation system being present, neither applications nor cancellations are possible!';
+$GLOBALS['TL_LANG']['MSC']['welcome_gantt']['task_description']['holiday']                         = 'It is holiday time. The calendar widgets use this dates.';
+$GLOBALS['TL_LANG']['MSC']['welcome_gantt']['task_description']['host_editing_stage']              = 'In the host editing stage, the hosts are allowed to create, edit and delete their offers.';
+$GLOBALS['TL_LANG']['MSC']['welcome_gantt']['task_description']['show_offers']                     = 'The Ferienpass offers get displayed in the frontend. If not active, an empty result set will get displayed in the frontend.';
+
+$GLOBALS['TL_LANG']['MSC']['ferienpass_erase_personal_data']['main_headline'] = 'You might use this tool to delete all parent\'s personal data on this website.';
