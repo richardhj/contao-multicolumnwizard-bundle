307ea4a7d464c2c4a9f4511c91670e247daa2fd9
Richard Henkenjohann <richardhenkenjohann@googlemail.com>
2019-01-17 12:46:28 +0100

    Update copyright.

--- tests/MetaModels/Attribute/Age/AttributeTypeFactoryTest.php
+++ tests/MetaModels/Attribute/Age/AttributeTypeFactoryTest.php
@@ -3,11 +3,11 @@
 /**
  * This file is part of richardhj/contao-ferienpass.
  *
- * Copyright (c) 2015-2018 Richard Henkenjohann
+ * Copyright (c) 2015-2019 Richard Henkenjohann
  *
  * @package   richardhj/contao-ferienpass
  * @author    Richard Henkenjohann <richardhenkenjohann@googlemail.com>
- * @copyright 2015-2018 Richard Henkenjohann
+ * @copyright 2015-2019 Richard Henkenjohann
  * @license   https://github.com/richardhj/contao-ferienpass/blob/master/LICENSE
  */
 
